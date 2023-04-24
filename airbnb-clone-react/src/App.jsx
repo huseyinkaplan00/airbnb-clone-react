93c6ca7346181fcb254fb04b5def6f74d370d111
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-04-24 14:22:34 +0300

    airbnb clone with react starts

---
+++ airbnb-clone-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Card from "./components/Card.jsx"
+import Hero from "./components/Hero.jsx"
+import Navbar from "./components/Navbar.jsx"
+
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<Card />
+		</>
+	)
+}
+
+export default App
