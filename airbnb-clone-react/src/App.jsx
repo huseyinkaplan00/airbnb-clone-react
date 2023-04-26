db81d04f7d8e81f6d99d923a7add3fec0307d562
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-04-26 13:33:45 +0300

    data and card components added

--- airbnb-clone-react/src/App.jsx
+++ airbnb-clone-react/src/App.jsx
@@ -2,14 +2,29 @@ import React from "react"
 import Card from "./components/Card.jsx"
 import Hero from "./components/Hero.jsx"
 import Navbar from "./components/Navbar.jsx"
-
+import data from './data.jsx'
 const App = () => {
+	const cards = data.map(items => {
+		return(
+			
+			<Card  
+			items= {items}
+			key={items.key}
+			
+			/>
+		)
+	}
+		)
 	return (
-		<>
+		
+		<div className="container__main">
 			<Navbar />
 			<Hero />
+			{/* <section className="card-list">
+				{cards}
+			</section> */}
 			<Card />
-		</>
+		</div>
 	)
 }
 
--- airbnb-clone-react/src/components/Card.jsx
+++ airbnb-clone-react/src/components/Card.jsx
@@ -1,5 +1,24 @@
 import React from "react"
 
-const Card = () => {}
+const Card = (props) => {
+    return(
+        <div className="card__container">
+
+           <div className="card__element">
+                <div className="badget">Sold Out && Online</div>
+                <img src="../public/katie-zaferes.png"></img>
+                <img src="../public/star.png"></img>
+                <span>5.0</span>
+                <span>(6) ● USA</span>
+                <p>Life lessons with Katie Zaferes</p>         
+                <p>From 136$ / <span>person</span></p>   
+            </div> 
+
+
+        </div>
+    )
+
+
+}
 
 export default Card
