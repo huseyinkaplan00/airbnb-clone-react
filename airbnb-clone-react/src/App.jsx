d66f5a6183fdb59d2244a5185322aac8cf9e6cd1
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-04-28 13:19:06 +0300

    ui changes

--- airbnb-clone-react/src/App.jsx
+++ airbnb-clone-react/src/App.jsx
@@ -2,28 +2,23 @@ import React from "react"
 import Card from "./components/Card.jsx"
 import Hero from "./components/Hero.jsx"
 import Navbar from "./components/Navbar.jsx"
-import data from './data.jsx'
+import Footer from "./components/Footer.jsx"
+import data from "./data.jsx"
 const App = () => {
-	const cards = data.map(items => {
-		return(
-			
-			<Card  
-			{...items}
-			key={items.key}
-			
+	const cards = data.map((items) => {
+		return (
+			<Card
+				{...items}
+				key={items.key}
 			/>
 		)
-	}
-		)
+	})
 	return (
-		
 		<div className="container__main">
 			<Navbar />
 			<Hero />
-			<section className="card-list">
-				{cards}
-			</section>
-			{/* <Card /> */}
+			<section className="card-list">{cards}</section>
+			<Footer />
 		</div>
 	)
 }
---
+++ airbnb-clone-react/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+	return (
+		<footer>
+			<small> Hüseyin Kaplan © 2023 </small>
+		</footer>
+	)
+}
+
+export default Footer
