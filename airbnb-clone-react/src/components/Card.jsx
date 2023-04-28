311574bad81059e9dabe8e56adb6727380e0fe59
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-04-28 17:00:40 +0300

    image path update

--- airbnb-clone-react/src/components/Card.jsx
+++ airbnb-clone-react/src/components/Card.jsx
@@ -1,27 +1,31 @@
 import React from "react"
 
 const Card = (props) => {
-
-    console.log(props)
-    return(
-       
-
-           <div className="card__section">
-                {/* <div className="badget">Sold Out && Online</div> */}
-                <img className="main--image" src={`/${props.coverImg}`}></img>
-             <div className="card-stats">
-                <img className="star--icon" src="/star.png" alt="start icon"></img> <span>{props.stats.rating}</span>
-                <span>({`${props.stats.reviewCount}`}) ● {`${props.location}`}</span>
-                <p>{props.title}</p>         
-                <p>From {`$${props.price}`} / <span>person</span></p>
-             </div>   
-            </div> 
-
-
-    
-    )
-
-
+	console.log(props)
+	return (
+		<div className="card__section">
+			{/* <div className="badget">Sold Out && Online</div> */}
+			<img
+				className="main--image"
+				src={`./${props.coverImg}`}
+			></img>
+			<div className="card-stats">
+				<img
+					className="star--icon"
+					src="./star.png"
+					alt="start icon"
+				></img>{" "}
+				<span>{props.stats.rating}</span>
+				<span>
+					({`${props.stats.reviewCount}`}) ● {`${props.location}`}
+				</span>
+				<p>{props.title}</p>
+				<p>
+					From {`$${props.price}`} / <span>person</span>
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default Card
